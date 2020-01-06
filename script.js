9e5d6de86a2a43bc8f496f751584d09177d3d4c2
Khinshan Khan <khanshan501@gmail.com>
2020-01-06 14:15:29 -0500

    fixed small error

--- script.js
+++ script.js
@@ -15,15 +15,15 @@ document.querySelector("ol .third");
 
 //5 - Give the section with an id of container the text "Hello!".
 
-document.getElementById("container").innerHTML = "Hello!";
+document.getElementById("container").prepend(document.createTextNode("Hello!"));
 
 //6 - Add the class main to the div with a class of footer.
 
-document.getElementsByClassName('footer')[0].classList.add('main');
+document.getElementsByClassName("footer")[0].classList.add("main");
 
 //7 - Remove the class main on the div with a class of footer.
 
-document.getElementsByClassName('footer')[0].classList.remove('main');
+document.getElementsByClassName("footer")[0].classList.remove("main");
 
 //8 - Create a new li element.
 
@@ -39,13 +39,12 @@ document.querySelector("ol").appendChild(li);
 
 //12 - Loop over all of the lis inside the ol tag and give them a background color of "green".
 
-var x = document.querySelectorAll("ol li"); 
+var x = document.querySelectorAll("ol li");
 
-for (let i = 0; i < x.length; i++)
-{
-    x[i].style.backgroundColor = "green"; 
+for (let i = 0; i < x.length; i++) {
+  x[i].style.backgroundColor = "green";
 }
 
 //13 - Remove the div with a class of footer.
 
-document.body.removeChild(document.querySelector('.footer'));
+document.body.removeChild(document.querySelector(".footer"));
